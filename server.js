70cc2a8d46456875457d3800eea32c8a062a4872
Nirmalya838 <122223948+Nirmalya838@users.noreply.github.com>
2023-05-19 00:14:03 +0530

    created server at localhost
    created response in different url in localhost

--- server.js
+++ server.js
@@ -1,7 +1,32 @@
 const http = require('http');
 
 const server = http.createServer((request, response) => {
-  response.end('My name is Nirmalya Sengupta');
+  const url = request.url;
+  if (request.url === "/home") {
+    response.write("<html>");
+    response.write("<head></head>");
+    response.write("<body><h1>Welcome Home</h1></body>");
+    response.write("</html>");
+    response.end();
+  } else if (request.url === "/about") {
+    response.write("<html>");
+    response.write("<head></head>");
+    response.write("<body><h1>Welcome to About Us page</h1></body>");
+    response.write("</html>");
+    response.end();
+  } 
+  else if (request.url === "/node") {
+    response.write("<html>");
+    response.write("<head></head>");
+    response.write("<body><h1>Welcome to my Node Js project</h1></body>");
+    response.write("</html>");
+    response.end();
+  }
+    else 
+    {
+    response.writeHead(404, { 'Content-Type': 'text/plain' });
+    response.end('Not found');
+    }
 });
 
 server.listen(4000, () => {
